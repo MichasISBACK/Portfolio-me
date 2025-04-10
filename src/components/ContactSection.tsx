3e01bb088fa18d85b36e50e9dca6c76235d33241
Luis Eduardo Carvalho <147669781+MichasISBACK@users.noreply.github.com>
2025-04-10 01:08:29 -0300

    Update ContactSection.tsx

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -145,7 +145,7 @@ const ContactSection = () => {
                       href={`https://${social}.com`}
                       target="_blank"
                       rel="noopener noreferrer"
-                      className="w-9 h-9 rounded-full bg-tech-black flex items-center justify-center hover:bg-primary hover:text-white transition-colors"
+                      className="w-9 h-9 rounded-full bg-tech-gray flex items-center justify-center hover:bg-primary hover:text-black transition-colors"
                     >
                       <span className="sr-only">{social}</span>
                       <div className="w-4 h-4"></div>
